499d927513454de0bd30ad229581eace53ae48b3
joannjmj48-ui <joann.jmj48@gmail.com>
2026-09-24 23:42:32 -0400

    f
    f

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline support. Vault files are encrypted, so caching them is safe.
-const SHELL = 'mt-shell-32d9214f1b';
+const SHELL = 'mt-shell-4b2db705fd';
 const ASSETS = 'mt-assets';
 const CORE = ['./', 'index.html', 'vault/app.bin', 'manifest.webmanifest', 'icon.svg', 'icon-180.png'];
 
