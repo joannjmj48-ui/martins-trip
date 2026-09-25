691fd2b098a8f61dcb3f743cf05276b79066797b
joannjmj48-ui <joann.jmj48@gmail.com>
2026-09-24 22:48:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline support. Vault files are encrypted, so caching them is safe.
-const SHELL = 'mt-shell-3b940c102d';
+const SHELL = 'mt-shell-870f6087a5';
 const ASSETS = 'mt-assets';
 const CORE = ['./', 'index.html', 'vault/app.bin', 'manifest.webmanifest', 'icon.svg', 'icon-180.png'];
 
